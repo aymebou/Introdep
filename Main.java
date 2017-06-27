ca622e48edd41ca562d942f2ee5f30c894ef5674
aymebou <a.bouzigues@outlook.fr>
2017-06-27 11:50:51 +0200

    Added working Git

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+public class Main {
 
+	public static void main(String[] args) { 	//void: on retourne rien. Permets un appel direct de la fonction,
+							//static : la fonction n'est rattachée à rien
+	for(int i = 0; i < 10 ; i++) {
+		System.out.println("Hello");
+	}
+
+	Foo c = new Foo();
+	c.increment();
+
+	System.out.println(c.getValeur());
+
+	System.out.println(Foo.disBonjour());
+
+	System.out.println(Math.PI);
+	}
+}
--- compteur.java
+++ compteur.java
@@ -1,16 +1,16 @@
 public class Compteur {
 
 	private int valeur = 0;
-	
+
 	public void increment() {
 		this.valeur += 1;
-	}	
+	}
 	public int getValeur {
 		return this.valeur;
 	}
-	
+
 	public static String disBonjour() { //static: fonction non rattachee à un objet.
-	
+
 	return "Bonjour";
 }
 
@@ -18,24 +18,7 @@ public class Compteur {
 
 
 
-public class Main {
 
-	public static void main(String[] args) { 	//void: on retourne rien. Permets un appel direct de la fonction, 
-							//static : la fonction n'est rattachée à rien
-	for(int i = 0; i < args.length ; i++) {
-		System.out.println("args[" + i + "] = " + args[i]);
-	}
-	
-	Foo c = new Foo();
-	c.increment();
-	
-	System.out.println(c.getValeur());
-	
-	System.out.println(Foo.disBonjour());
-	
-	System.out.println(Math.PI);
-	}
-}
 
 /*mot clé final: personne ne peut dire un objet est un maths
 heritage: un cheval est un animal. les objets d'une certaines classe sont des objets d'une autre classe
@@ -44,4 +27,3 @@ final: personne ne peut modifier
 */
 
 // On fait attention à la syntaxe (;,{}) sinon java pas content
-
