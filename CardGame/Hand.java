b147c3fc68a8d18c94824cb6f9ca0664a972725b
aymebou <a.bouzigues@outlook.fr>
2017-06-28 10:37:40 +0200

    Classe Hand

--- CardGame/Hand.java
+++ CardGame/Hand.java
@@ -1,8 +1,21 @@
-public calss Hand {
+import java.util.*;
+
+public class Hand {
 
   private int number;
-  private List<>
-  public play(card) {
+  private List <Card> cards ;
 
+public void Hand(int n , List <Card> c) {
+  this.number = n;
+  this.cards = c;
+}
+
+public void addCard (Card c){
+  this.number++;
+  this.cards.add(c);
   }
+public void removeCard(Card c){
+  if(this.cards.contains(c)) { this.cards.remove(c); this.number--;}
+
+}
 }
