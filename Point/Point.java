3e567ced7b2c8401b8d48bd10d39e7dc8e65fcfc
aymebou <a.bouzigues@outlook.fr>
2017-06-28 08:33:00 +0200

    Ajout de Point

---
+++ Point/Point.java
@@ -0,0 +1,19 @@
+public class Point {
+ public deplacer(int dx int dy) {
+
+
+ }
+ /* Calcule la distance du pt pr a l'origine */
+  float distance() {
+    
+
+  }
+
+  /* Calcule la distance du pt pr a un autre pt */
+  float distance (Point autre point) {
+
+
+  }
+
+
+}
